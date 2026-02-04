75fef582b5094016e2449cd8fea90fcebe72f156
aatmunbaxi <abaxi@tamu.edu>
2026-02-04 13:29:59 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based Jupyter development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ poetry hugo sage python315 ] ++ (with python315Packages; [
+            sage
+          ]);
+          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib";
+        };
+      });
+    };
+}
